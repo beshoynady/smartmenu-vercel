d159a98f766d5c4ed1a955249d9c68de9c51842b
beshoynady <bishoin2017@gmail.com>
2025-07-18 13:17:52 +0300

    Restaurant.router - Product.router -

--- server/router/Product.router.js
+++ server/router/Product.router.js
@@ -19,116 +19,124 @@ const {
 
 
 
-const imagesDir = path.join(__dirname, '..', 'images');
+// const imagesDir = path.join(__dirname, '..', 'images');
 
-if (!fs.existsSync(imagesDir)) {
-  fs.mkdirSync(imagesDir, { recursive: true });
-}
+// if (!fs.existsSync(imagesDir)) {
+//   fs.mkdirSync(imagesDir, { recursive: true });
+// }
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, imagesDir); 
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname;
-    cb(null, uniqueSuffix);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, imagesDir); 
+//   },
+//   filename: (req, file, cb) => {
+//     const uniqueSuffix = new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname;
+//     cb(null, uniqueSuffix);
+//   },
+// });
 
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1024 * 1024, // تحديد الحد الأقصى لحجم الملف بـ 1 ميجابايت
-  },
-  fileFilter: (req, file, cb) => {
-    // السماح بأنواع الصور فقط
-    const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg'];
-    if (allowedTypes.includes(file.mimetype)) {
-      cb(null, true);
-    } else {
-      cb(new Error('Only JPEG, JPG, and PNG file types are allowed.'));
-    }
-  },
-});
+// const upload = multer({
+//   storage: storage,
+//   limits: {
+//     fileSize: 1024 * 1024, // تحديد الحد الأقصى لحجم الملف بـ 1 ميجابايت
+//   },
+//   fileFilter: (req, file, cb) => {
+//     // السماح بأنواع الصور فقط
+//     const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg'];
+//     if (allowedTypes.includes(file.mimetype)) {
+//       cb(null, true);
+//     } else {
+//       cb(new Error('Only JPEG, JPG, and PNG file types are allowed.'));
+//     }
+//   },
+// });
 
-// إدارة الصور القديمة
-const deleteOldImage = (imagePath) => {
-  if (fs.existsSync(imagePath)) {
-    fs.unlinkSync(imagePath);
-    console.log('Old image deleted successfully');
-  }
-};
+// // إدارة الصور القديمة
+// const deleteOldImage = (imagePath) => {
+//   if (fs.existsSync(imagePath)) {
+//     fs.unlinkSync(imagePath);
+//     console.log('Old image deleted successfully');
+//   }
+// };
 
-// Middleware لحذف الصورة القديمة إذا كانت هناك صورة جديدة يتم رفعها
-const deleteOldImageMiddleware = async (req, res, next) => {
-  try {
-    console.log('Middleware triggered');
-    const productId = req.params.productId;
-    if (!productId) {
-      return res.status(400).json({ message: 'Product ID is missing' });
-    }
+// // Middleware لحذف الصورة القديمة إذا كانت هناك صورة جديدة يتم رفعها
+// const deleteOldImageMiddleware = async (req, res, next) => {
+//   try {
+//     console.log('Middleware triggered');
+//     const productId = req.params.productId;
+//     if (!productId) {
+//       return res.status(400).json({ message: 'Product ID is missing' });
+//     }
 
-    // استدعاء getOneProduct بشكل صحيح
-    const productResponse = await getProduct({ params: { productId: productId } }, res);
-    const product = productResponse.product;  // افترض أن getOneProduct تعيد product داخل response
+//     // استدعاء getOneProduct بشكل صحيح
+//     const productResponse = await getProduct({ params: { productId: productId } }, res);
+//     const product = productResponse.product;  // افترض أن getOneProduct تعيد product داخل response
 
-    if (!product) {
-      return res.status(404).json({ message: 'Product not found' });
-    }
+//     if (!product) {
+//       return res.status(404).json({ message: 'Product not found' });
+//     }
 
-    if (product.image && req.file) {
-      const oldImagePath = path.join(imagesDir, product.image);
-      console.log('Deleting old image:', oldImagePath);
-      deleteOldImage(oldImagePath);
-    }
+//     if (product.image && req.file) {
+//       const oldImagePath = path.join(imagesDir, product.image);
+//       console.log('Deleting old image:', oldImagePath);
+//       deleteOldImage(oldImagePath);
+//     }
 
-    next();
-  } catch (err) {
-    console.error('Error deleting old image', err);
-    return res.status(500).json({ message: 'Internal server error' });
-  }
-};
+//     next();
+//   } catch (err) {
+//     console.error('Error deleting old image', err);
+//     return res.status(500).json({ message: 'Internal server error' });
+//   }
+// };
 
-const deleteImageProductMiddleware = async (req, res, next) => {
-  try {
-    console.log('Middleware triggered');
-    const productId = req.params.productId;
-    if (!productId) {
-      return res.status(400).json({ message: 'Product ID is missing' });
-    }
+// const deleteImageProductMiddleware = async (req, res, next) => {
+//   try {
+//     console.log('Middleware triggered');
+//     const productId = req.params.productId;
+//     if (!productId) {
+//       return res.status(400).json({ message: 'Product ID is missing' });
+//     }
 
-    // استدعاء getOneProduct بشكل صحيح
-    const productResponse = await getProduct({ params: { productId: productId } }, res);
-    const product = productResponse.product;  
-    if (!product) {
-      return res.status(404).json({ message: 'Product not found' });
-    }
+//     // استدعاء getOneProduct بشكل صحيح
+//     const productResponse = await getProduct({ params: { productId: productId } }, res);
+//     const product = productResponse.product;  
+//     if (!product) {
+//       return res.status(404).json({ message: 'Product not found' });
+//     }
 
-    if (product.image) {
-      const oldImagePath = path.join(imagesDir, product.image);
-      console.log('Deleting old image:', oldImagePath);
-      deleteOldImage(oldImagePath);
-    }
+//     if (product.image) {
+//       const oldImagePath = path.join(imagesDir, product.image);
+//       console.log('Deleting old image:', oldImagePath);
+//       deleteOldImage(oldImagePath);
+//     }
 
-    next();
-  } catch (err) {
-    console.error('Error deleting old image', err);
-    return res.status(500).json({ message: 'Internal server error' });
-  }
-};
+//     next();
+//   } catch (err) {
+//     console.error('Error deleting old image', err);
+//     return res.status(500).json({ message: 'Internal server error' });
+//   }
+// };
 
 
+// router.route('/')
+//   .post(authenticateToken, checkSubscription, upload.single("image"), createProduct)
+//   .get(getAllProducts);
 router.route('/')
-  .post(authenticateToken, checkSubscription, upload.single("image"), createProduct)
+  .post(authenticateToken, checkSubscription, createProduct)
   .get(getAllProducts);
 
 router.route('/getproductbycategory/:categoryid')
   .get(getProductByCategory);
 
+// router.route('/:productId')
+//   .get(getOneProduct)
+//   .put(authenticateToken, checkSubscription, upload.single("image"), deleteOldImageMiddleware, updateProduct)
+//   .delete(authenticateToken, checkSubscription, deleteImageProductMiddleware, deleteProduct);
+
 router.route('/:productId')
   .get(getOneProduct)
-  .put(authenticateToken, checkSubscription, upload.single("image"), deleteOldImageMiddleware, updateProduct)
-  .delete(authenticateToken, checkSubscription, deleteImageProductMiddleware, deleteProduct);
+  .put(authenticateToken, checkSubscription, updateProduct)
+  .delete(authenticateToken, checkSubscription, deleteProduct);
 
 
 module.exports = router;
--- server/router/Restaurant.router.js
+++ server/router/Restaurant.router.js
@@ -17,93 +17,102 @@ const {
 const {authenticateToken} = require("../utlits/authenticate");
 const checkSubscription = require("../utlits/checkSubscription");
 
-const imagesDir = path.join(__dirname, "..", "images");
+// const imagesDir = path.join(__dirname, "..", "images");
 
-if (!fs.existsSync(imagesDir)) {
-  fs.mkdirSync(imagesDir, { recursive: true });
-}
+// if (!fs.existsSync(imagesDir)) {
+//   fs.mkdirSync(imagesDir, { recursive: true });
+// }
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, imagesDir);
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix =
-      new Date().toISOString().replace(/:/g, "-") + "-" + file.originalname;
-    cb(null, uniqueSuffix);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, imagesDir);
+//   },
+//   filename: (req, file, cb) => {
+//     const uniqueSuffix =
+//       new Date().toISOString().replace(/:/g, "-") + "-" + file.originalname;
+//     cb(null, uniqueSuffix);
+//   },
+// });
 
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1024 * 1024, // تحديد الحد الأقصى لحجم الملف بـ 1 ميجابايت
-  },
-  fileFilter: (req, file, cb) => {
-    // السماح بأنواع الصور فقط
-    const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
-    if (allowedTypes.includes(file.mimetype)) {
-      cb(null, true);
-    } else {
-      cb(new Error("Only JPEG, JPG, and PNG file types are allowed."));
-    }
-  },
-});
+// const upload = multer({
+//   storage: storage,
+//   limits: {
+//     fileSize: 1024 * 1024, // تحديد الحد الأقصى لحجم الملف بـ 1 ميجابايت
+//   },
+//   fileFilter: (req, file, cb) => {
+//     // السماح بأنواع الصور فقط
+//     const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
+//     if (allowedTypes.includes(file.mimetype)) {
+//       cb(null, true);
+//     } else {
+//       cb(new Error("Only JPEG, JPG, and PNG file types are allowed."));
+//     }
+//   },
+// });
 
-const deleteOldImage = (imagePath) => {
-  if (fs.existsSync(imagePath)) {
-    fs.unlinkSync(imagePath);
-    console.log("Old image deleted successfully");
-  }
-};
+// const deleteOldImage = (imagePath) => {
+//   if (fs.existsSync(imagePath)) {
+//     fs.unlinkSync(imagePath);
+//     console.log("Old image deleted successfully");
+//   }
+// };
 
-const deleteOldImageMiddleware = async (req, res, next) => {
-  try {
-    console.log("Middleware triggered");
-    const restaurantId = req.params.id;
-    if (!restaurantId) {
-      return res.status(400).json({ message: "Product ID is missing" });
-    }
+// const deleteOldImageMiddleware = async (req, res, next) => {
+//   try {
+//     console.log("Middleware triggered");
+//     const restaurantId = req.params.id;
+//     if (!restaurantId) {
+//       return res.status(400).json({ message: "Product ID is missing" });
+//     }
 
-    // استدعاء getOneProduct بشكل صحيح
-    const restaurantResponse = await getRestaurant(
-      { params: { id: restaurantId } },
-      res
-    );
-    const restaurant = restaurantResponse.restaurant;
+//     // استدعاء getOneProduct بشكل صحيح
+//     const restaurantResponse = await getRestaurant(
+//       { params: { id: restaurantId } },
+//       res
+//     );
+//     const restaurant = restaurantResponse.restaurant;
 
-    if (!restaurant) {
-      return res
-        .status(404)
-        .json({ message: "restaurant not found to delete OldImage" });
-    }
+//     if (!restaurant) {
+//       return res
+//         .status(404)
+//         .json({ message: "restaurant not found to delete OldImage" });
+//     }
 
-    if (restaurant.image && req.file) {
-      const oldImagePath = path.join(imagesDir, restaurant.image);
-      console.log("Deleting old image:", oldImagePath);
-      deleteOldImage(oldImagePath);
-    }
+//     if (restaurant.image && req.file) {
+//       const oldImagePath = path.join(imagesDir, restaurant.image);
+//       console.log("Deleting old image:", oldImagePath);
+//       deleteOldImage(oldImagePath);
+//     }
 
-    next();
-  } catch (err) {
-    console.error("Error deleting old image", err);
-    return res.status(500).json({ message: "Internal server error" });
-  }
-};
+//     next();
+//   } catch (err) {
+//     console.error("Error deleting old image", err);
+//     return res.status(500).json({ message: "Internal server error" });
+//   }
+// };
 
+// router
+//   .route("/")
+//   .post(authenticateToken, upload.single("image"), createRestaurant)
+//   .get(getAllRestaurants);
 router
   .route("/")
-  .post(authenticateToken, upload.single("image"), createRestaurant)
+  .post(authenticateToken, createRestaurant)
   .get(getAllRestaurants);
 
 router
   .route("/:id")
   .get(authenticateToken, checkSubscription, getRestaurantById)
+  // .put(
+  //   authenticateToken,
+  //   checkSubscription,
+  //   upload.single("image"),
+  //   deleteOldImageMiddleware,
+  //   updateRestaurant
+  // )
   .put(
     authenticateToken,
     checkSubscription,
-    upload.single("image"),
-    deleteOldImageMiddleware,
     updateRestaurant
   )
   .delete(authenticateToken, checkSubscription, deleteRestaurant);
